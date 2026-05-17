b0ba0f436bf53283e407ac0fe12499f25286ff16
Smarty-Samie <inbox@ampliserv.ru>
2026-05-18 00:12:02 +0300

    тестируем работу гита

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <string.h>
 
-int main(void)
+int main(void)// тест гита...
 {
-    printf("Hello World!\n");
+    //printf("Hello World!\n");
     return 0;
 }
 
